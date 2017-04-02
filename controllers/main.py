2b7d85663a7e501141897f054e5b338325c3fbc4
lisjin <lisajin@umich.edu>
2017-04-02 19:35:42 -0400

    Interactive bar graphs by threshold, # time steps

--- controllers/main.py
+++ controllers/main.py
@@ -1,9 +1,36 @@
 from flask import *
+from collections import Counter
 
 import os
 import json
 
 main = Blueprint('main', __name__, template_folder='templates')
+
+
+def get_strucs(path, entry):
+	strucs = {}
+	strucs_flat = []
+
+	for e in entry:
+		strucs.setdefault(e['struc'], 0)
+		strucs[e['struc']] += 1
+	for k, v in strucs.iteritems():
+		strucs_flat.append({'category': k, 'amount': v})
+	return strucs_flat
+
+
+def get_nodes(path, entry):
+	nodes = {}
+	nodes_flat = []
+	
+	for e in entry:
+		for n in e['nodes']:
+			nodes.setdefault(n, 0)
+			nodes[n] += 1
+	count = Counter(nodes.values())
+	for k in count:
+		nodes_flat.append({'category': int(k), 'amount': count[k]})
+	return nodes_flat
 
 
 @main.route('/')
@@ -13,29 +40,16 @@ def main_route():
 
 @main.route('/data/<path:path>')
 def send_data(path):
-	type = request.args.get('type')
-	if type == 'struc_distr':
-		strucs = {}
-		strucs_flat = []
-		entry = json.load(open(os.path.join(os.path.realpath(os.path.dirname(__file__)), '..', 'data', path)))
-		for e in entry:
-			strucs.setdefault(e['struc'], 0)
-			strucs[e['struc']] += 1
-		for k, v in strucs.iteritems():
-			strucs_flat.append({'category': k, 'amount': v})
+	req_type = request.args.get('type')
+	entry = json.load(open(os.path.join(os.path.realpath(os.path.dirname(__file__)), '..', 'data', path)))
+
+	if req_type == 'struc_distr':
+		strucs_flat = get_strucs(path, entry)
 		return json.dumps(sorted(strucs_flat, key=lambda k: k['amount'], reverse=True))
-	elif type == 'node_distr':
-		nodes = {}
-		nodes_flat = []
-		entry = json.load(open(os.path.join(os.path.realpath(os.path.dirname(__file__)), '..', 'data', path)))
-		for e in entry:
-			for n in e['nodes']:
-				nodes.setdefault(n, 0)
-				nodes[n] += 1
-		for k, v in nodes.iteritems():
-			nodes_flat.append({'category': int(k), 'amount': v})
-		return json.dumps(sorted(nodes_flat, key=lambda k: k['amount'], reverse=True))
-	return send_from_directory('data', path, mimetype='text/html')
+	elif req_type == 'node_distr':
+		nodes_flat = get_nodes(path, entry)
+		return json.dumps(sorted(nodes_flat, key=lambda k: k['category']))
+	abort(404)
 
 
 @main.route('/static/<path:path>')
