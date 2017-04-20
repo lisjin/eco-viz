e969ea1d0efcc34b98fa4762a63bf2186f78e43c
lisjin <lisajin@umich.edu>
2017-04-19 23:30:51 -0400

    Two-column layout for rest/mindful rest

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,4 @@
-function updateEmbed(spec_v1_url, spec_v2_url) {
+function updateEmbed(spec_v1_url, spec_v2_url, spec_v3_url, spec_v4_url) {
 	$.getJSON('static/specs/spec_v1.json', function(spec) {
 		spec.data[0].url = spec_v1_url;
 		vega.embed('#view1', spec);
@@ -7,17 +7,25 @@ function updateEmbed(spec_v1_url, spec_v2_url) {
 		spec.data[0].url = spec_v2_url;
 		vega.embed('#view2', spec);
 	});
+	$.getJSON('static/specs/spec_v3.json', function(spec) {
+		spec.data[0].url = spec_v3_url;
+		vega.embed('#view3', spec);
+	});
+	$.getJSON('static/specs/spec_v4.json', function(spec) {
+		spec.data[0].url = spec_v4_url;
+		vega.embed('#view4', spec);
+	});
 }
 
-function updateTable(data_url) {
+function updateTable(data_url, table_templ_id, table_id) {
 	$.getJSON(data_url, function(data) {
-		var output = $('#tc-table');
-		var template = $('#tc-table-template').html()
+		var output = $('#' + table_id);
+		var template = $('#' + table_templ_id).html()
 		var test_out = Mustache.render(template, {"strucs": data, toFixed: function() {
 			return function(num, render) { return parseFloat(render(num)).toFixed(4); }
 		}});
-		$('#tc-table-data').remove();
-		$('#tc-table').append(test_out);
+		$('#' + table_id + '-data').remove();
+		$('#' + table_id).append(test_out);
 	});
 }
 
@@ -26,17 +34,25 @@ function tcInputListener() {
 	var tstep = $('#tc-input-tstep').find('option:selected')[0].value;
 
 	var pref = 'data/pos_' + thresh + '-' + tstep + '.json'
+	var pref2 = 'data/pos_MR_' + thresh + '-' + tstep + '.json'
 	var spec_v1_url = pref + '?type=struc_distr'
 	var spec_v2_url = pref + '?type=node_distr'
+	var spec_v3_url = pref2 + '?type=struc_distr'
+	var spec_v4_url = pref2 + '?type=node_distr'
 
-	updateEmbed(spec_v1_url, spec_v2_url);
-	updateTable(pref);
+	updateEmbed(spec_v1_url, spec_v2_url, spec_v3_url, spec_v4_url);
+	updateTable(pref, 'tc-table-template', 'tc-table');
+	updateTable(pref2, 'tc-table2-template', 'tc-table2');
 }
 
 $(function() {
 	vega.embed('#view1', 'static/specs/spec_v1.json');
 	vega.embed('#view2', 'static/specs/spec_v2.json');
-	updateTable('data/pos_0.30-12.json');
+	vega.embed('#view3', 'static/specs/spec_v3.json');
+	vega.embed('#view4', 'static/specs/spec_v4.json');
+
+	updateTable('data/pos_0.30-12.json', 'tc-table-template', 'tc-table');
+	updateTable('data/pos_MR_0.30-12.json', 'tc-table2-template', 'tc-table2')
 
 	$('#tc-input-thresh').change(tcInputListener);
 	$('#tc-input-tstep').change(tcInputListener);
