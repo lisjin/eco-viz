1ab838ed256cfc98ea7c304f7cfdbfa33c121f60
lisjin <lisajin@umich.edu>
2017-05-07 20:56:16 -0400

    Small formatting changes

--- app.py
+++ app.py
@@ -8,5 +8,6 @@ app = Flask(__name__, template_folder='templates')
 app.register_blueprint(controllers.main)
 app.register_blueprint(controllers.api)
 
+# Must enable multithreading to support /api/traverse, which makes GET request to same server
 if __name__ == '__main__':
 	app.run(host=config.env['host'], port=config.env['port'], threaded=True, debug=True)
--- arango-scripts/drop.sh
+++ arango-scripts/drop.sh
@@ -2,16 +2,16 @@
 
 for g in ./*
 do
-if [[ $g =~ "edges" ]]
-then
-g=${g##*/}
-g=${g%_edges.json}
-DB=tc-viz
-GRAPH_NAME=$g
+	if [[ $g =~ "edges" ]]
+	then
+		g=${g##*/}
+		g=${g%_edges.json}
+		DB=tc-viz
+		GRAPH_NAME=$g
 
-arangosh --server.username "root" --server.password "" --server.database "$DB" --quiet << EOF
-var graph_module = require("@arangodb/general-graph");
-graph_module._drop("$GRAPH_NAME", true);
+		arangosh --server.username "root" --server.password "" --server.database "$DB" --quiet <<EOF
+		var graph_module = require("@arangodb/general-graph");
+		graph_module._drop("$GRAPH_NAME", true);
 EOF
-fi
+	fi
 done
